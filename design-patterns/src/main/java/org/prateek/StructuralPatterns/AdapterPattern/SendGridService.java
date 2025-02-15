97b68ec0a6eeb49a862b6d4e6f21950b3e33a7bc
Dhruvil_Soni <dhruvilsoni1703@gmail.com>
2025-02-15 22:09:40 +0530

    add code for StructuralPatterns in cpp

--- design-patterns/src/main/java/org/prateek/StructuralPatterns/AdapterPattern/SendGridService.java
+++ design-patterns/src/main/java/org/prateek/StructuralPatterns/AdapterPattern/SendGridService.java
@@ -2,8 +2,8 @@ package org.prateek.StructuralPatterns.AdapterPattern;
 
 public class SendGridService {
     public void sendEmail(String recipient, String title, String content){
-        System.out.println("Sending email via SendGrid to" + recipient);
-        System.out.println("Title : " + title);
-        System.out.println("Content : " + content);
+        System.out.println("Sending email via SendGrid to " + recipient);
+        System.out.println("Title: " + title);
+        System.out.println("Content: " + content);
     }
 }
